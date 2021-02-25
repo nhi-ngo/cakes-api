339b069d7dc71ae4606bab861ceff44313d69781
nhi-ngo <nhingo.y@gmail.com>
2021-02-25 11:23:29 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>Cakes API</h1>
+    </div>
+  );
+}
+
+export default App;
