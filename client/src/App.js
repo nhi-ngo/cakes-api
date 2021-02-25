68d4dd1a7db4185200eec1ef9bb67a67c37ae5fa
nhi-ngo <nhingo.y@gmail.com>
2021-02-25 15:56:38 -0500

    add client

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,43 @@ import style from './App.module.css'
 
 function App() {
 
+  // const [cake, setCake] = useState({})
+
+  // useEffect(() => {
+  //   fetch('https://my-cake-server.herokuapp.com/api/cake')
+  //     .then(res => res.json())
+  //     .then(item => setCake(item))
+  // }, [])
+
   return (
-    <div className={style.container}>
-      <h1 className={style.title}>Cakes API</h1>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <h1 className={style.title}>It’s cake o’clock</h1>
+
+        <h2 className={style.endPointsHeader}>Endpoints</h2>
+
+      <section className={style.sectionContainer}>
+        <h4><em>cake:</em> endpoint pertaining to a single random cake recipe</h4>
+
+        <p className={style.sectionRow}>
+          <span>get</span>
+          <a className={style.sectionLink} href = "https://my-cake-server.herokuapp.com/api/cake" >
+            /api/cake
+          </a>
+        </p>
+      </section>
+
+      <section className={style.sectionContainer}>
+        <h4><em>all-cakes</em>: endpoint pertaining to an array of all cake recipes</h4>
+
+        <p className={style.sectionRow}>
+          <span>get</span>
+          <a className={style.sectionLink} href = "https://my-cake-server.herokuapp.com/api/allcakes" >
+            /api/allcakes
+          </a>
+        </p>
+      </section>
+      </div>
     </div>
   );
 }
